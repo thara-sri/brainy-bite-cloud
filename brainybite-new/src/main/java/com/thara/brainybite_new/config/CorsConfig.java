0c1a2810d6c6864b5511c2d73466364c633be9ec
thara <sunsummerone1@gmail.com>
2026-03-18 00:19:16 +0700

    update CROS policy

--- brainybite-new/src/main/java/com/thara/brainybite_new/config/CorsConfig.java
+++ brainybite-new/src/main/java/com/thara/brainybite_new/config/CorsConfig.java
@@ -1,15 +1,20 @@
 package com.thara.brainybite_new.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Only path beginning with /api/ are allowed.
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
